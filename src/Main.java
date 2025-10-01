088890524c43bc8958e2cef86578f1b9b3a49517
Erik <3kdapp25.norder@folkuniversitetet.nu>
2025-10-01 15:06:18 +0200

    Added a printBoard function to print the current board

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
+
+    public static String[][] board = {{" "," ", " "},{" ", " "," "},{" ", " ", " "}};
+
     public static void main(String[] args) {
 
+        board[0][0] = "O";
+        board[0][1] = "X";
+        board[0][2] = " ";
+        board[1][0] = " ";
+        board[1][1] = "O";
+        board[1][2] = " ";
+        board[2][0] = " ";
+        board[2][1] = " ";
+        board[2][2] = "X";
+
+        printBoard();
+
+    }
+
+    public static void printBoard() {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                System.out.print(" " + board[i][j] + ((j == 2) ? "" : " |"));
+
+            }
+            System.out.println();
+            System.out.println((i == 2) ? "" : "---+---+---");
+        }
     }
 }
