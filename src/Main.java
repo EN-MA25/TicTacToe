6fc35b0fb4bc8f82a075c17617ab8c4c3a4fa6eb
Erik <3kdapp25.norder@folkuniversitetet.nu>
2025-10-09 11:44:45 +0200

    Added an input handler class to handle user input

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,13 @@
 
-import java.util.HashSet;
-import java.util.Scanner;
-
 public class Main {
 
     static Model model = new Model();
 
-    public static HashSet<String> allowedPlaces = new HashSet<>();
 
     public static void main(String[] args) {
 
 
-
-        allowedPlaces.add("A1");
-        allowedPlaces.add("A2");
-        allowedPlaces.add("A3");
-        allowedPlaces.add("B1");
-        allowedPlaces.add("B2");
-        allowedPlaces.add("B3");
-        allowedPlaces.add("C1");
-        allowedPlaces.add("C2");
-        allowedPlaces.add("C3");
-        allowedPlaces.add("quit");
-
-        Scanner input = new Scanner(System.in);
+        InputHandler input = new InputHandler();
 
         int playerTurn = 1;
         int turns = 0;
@@ -31,12 +15,11 @@ public class Main {
             clearScreen();
             printBoard();
             System.out.println("Player " + numberToString(playerTurn) + " turn");
-            String text = input.nextLine();
+            String text = input.getString();
 
-            if (allowedPlaces.contains(text)) {
-                if (text.equals("quit")) {
-                    System.exit(0);
-                }
+
+            if (text != null) {
+
                 Position pos = cordinateToPosition(text);
 
                 if (model.getValue(pos) == 0) {
@@ -112,11 +95,5 @@ public class Main {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
-
-
-
-
-
-
 
 }
